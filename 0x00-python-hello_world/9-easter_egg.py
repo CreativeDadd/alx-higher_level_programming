6afb70364ab8ba9610c2957b1a688cc5a3d80b12
CreativeDadd <oyebolang@gmail.com>
2022-12-04 03:16:57 -0800

    9-easter_egg

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = ''' The Zen of Python, by Tim Peters
+str =""" The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -15,8 +15,8 @@ In the face of ambiguity, refuse the temptation to guess.
 There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
 Now is better than never.
-Although never is often better than/ *right* now.
+Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- lets do more of those!'''
+Namespaces are one honking great idea -- lets do more of those!"""
 print(str)
